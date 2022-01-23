8684f1ea759e468bec37175a19b9bd7f25092a62
Seb <sebcyde@hotmail.com>
2022-01-23 15:48:36 +0000

    started typing out game user win scenario logic

--- Rock_Paper_Scissors.py
+++ Rock_Paper_Scissors.py
@@ -12,4 +12,18 @@ while True:
     if user_input not in options:
         continue
 
+    random_number = random.randint(0,2)
+    # Rock:0 , Paper:1 , Scissors:2
+
+    if user_input == options[0] and random_number == options[2]:
+        print("Computer chose scissors, you win!")
     
+    elif user_input == options[1] and random_number == options[0]:
+        print("Computer chose rock, you win!")
+        
+    elif user_input == options[2] and random_number == options[1]:
+        print("Computer chose paper, you win!")
+
+
+
+print("Goodbye!")
