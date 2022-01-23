9793e3e6438b9c197f6a3db6e3ea90301f264c01
Seb <sebcyde@hotmail.com>
2022-01-23 15:42:22 +0000

    created initial variables and started writing user input logic

---
+++ Rock_Paper_Scissors.py
@@ -0,0 +1,15 @@
+import random
+
+user_wins = 0
+computer_wins = 0
+options = ["rock", "paper", "scissors"]
+
+while True:
+    user_input = input("Type Rock/Paper/Scissors or Q to quit: ").lower()
+    if user_input == "q":
+        break
+
+    if user_input not in options:
+        continue
+
+    
